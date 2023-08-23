15aa5ab0b2518cd641bfbbf6514463ac2436be2f
Akash santhosh <akash777kk@gmail.com>
2023-08-23 12:06:59 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace PocConfigFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string valueString1 = System.Configuration.ConfigurationManager.AppSettings["id1"];
+            string valueString2 = System.Configuration.ConfigurationManager.AppSettings["id2"];
+            Console.WriteLine(valueString1);
+            Console.WriteLine(valueString2);
+
+            var valueApp = ConfigurationManager.AppSettings;
+            foreach (var keyMade in valueApp.AllKeys) 
+            {
+                string valueName = valueApp[keyMade];
+                Console.WriteLine(keyMade);
+                Console.WriteLine(valueName);
+                
+            }
+        }
+    }
+}
